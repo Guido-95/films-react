b0d0c42b49c61c7c190d1ba6a54b99ad005cdd5e
Guido-95 <guidocarna1995@gmail.com>
2022-04-20 22:58:40 +0200

    film react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React,{useState} from 'react'
 import '../scss/header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass   } from '@fortawesome/free-solid-svg-icons'
 import { useContextGlobal } from '../context/AppProvider';
+
 const Header = () => {
     const {fetchFilms} = useContextGlobal();
     const [film,setFilm] = useState('');
     const impostaNomeFilm = (e)=>{
+        
         setFilm(e.target.value)
         if (e.key === 'Enter') {
             fetchFilms(film);
@@ -13,11 +17,21 @@ const Header = () => {
     }
    
     return (
-        <header>
-            <h1 className='logo'>Logo</h1>
-            <div className="ricerca">
-                <input type="text" onKeyDown={impostaNomeFilm} onChange= {impostaNomeFilm} value={film} />
-                <button onClick={()=>fetchFilms(film)} className='bottone-ricerca btn btn-info'>cerca...</button>
+        <header id='header'>
+            <h1 className='logo'> 
+                <a href='http://localhost:3000/'>
+                    React Films
+                </a> 
+            </h1>
+          
+            <div id='div-ricerca' className="ricerca">
+               
+                <form id='form-ricerca' onSubmit={e => {e.preventDefault();}}>
+                    <input type="text" placeholder='scrivi un titolo..' onKeyDown={impostaNomeFilm} onChange= {impostaNomeFilm} value={film} />
+                    <FontAwesomeIcon onClick={()=>fetchFilms(film)} icon={faMagnifyingGlass} />
+                  
+                </form>
+                
             </div>
         </header>
     )
--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -12,27 +12,50 @@ const initialState = {
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer,initialState)
     const fetchFilms = async(nomeFilm)=>{
-        dispatch({
-            type:START_FETCH
-        })
-        try{
-            const response = await axios.get('https://api.themoviedb.org/3/search/movie',{ 
-                params:{
-                api_key: "e99307154c6dfb0b4750f6603256716d",
-                query: nomeFilm,
-                language: 'it-IT'
-                }
-            })
-            dispatch({
-                type: CERCA_FILM,
-                payload: response.data.results,
-            });
+        
+        if(!nomeFilm == '' && nomeFilm.trim().length > 1){
             
-        }catch(err){
-            console.log(err);
-        }  
+            if(document.getElementById('div-errore')){
+                let divForm = document.getElementById("div-ricerca");
+                let messaggioErrore = document.getElementById('div-errore');
+                divForm.removeChild(messaggioErrore);
+            }
+           
+            dispatch({
+                type:START_FETCH
+            })
+            try{
+                const response = await axios.get('https://api.themoviedb.org/3/search/movie',{ 
+                    params:{
+                    api_key: "e99307154c6dfb0b4750f6603256716d",
+                    query: nomeFilm,
+                    language: 'it-IT'
+                    }
+                })
+                dispatch({
+                    type: CERCA_FILM,
+                    payload: response.data.results,
+                });
+                
+            }catch(err){
+                console.log(err);
+            }  
+        }else{
+            if(! document.getElementById('div-errore')){
+                const elemento = document.createElement("div");
+                elemento.id = 'div-errore'
+                elemento.innerText = "Inserisci un titolo valido";
+                let divForm = document.getElementById("div-ricerca");
+                let form = document.getElementById('form-ricerca')
+                divForm.insertBefore(elemento, form);
+                // document.body.appendChild(para);
+            }
+      
+            
+        }
+       
     }
-    
+
     return (
         <AppContext.Provider
         value={{
